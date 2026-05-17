14fb354124c4009d37baf6b3048d435dbc47fee6
Brennan Rivera <brennanrivera0@gmail.com>
2026-05-17 16:25:00 -0400

    Fix broken build because of failed closing curly and paren

--- src/lib/bittorrent/bencode/TorrentManager.cpp
+++ src/lib/bittorrent/bencode/TorrentManager.cpp
@@ -77,6 +77,7 @@ void TorrentManager::grabMetaInfo(std::unordered_map<std::string, bittorrent::Be
                 meta_info_.files.emplace_back({
                     .length: std::get<uint64_t>(length_it->second.value),
                     .path:   std::get<std::string>(path_it->second.value)});
+        });
     }
     else
     {
