6565fb369b0d3cafbf263dcd1a1ed7507c3306f9
Brennan Rivera <brennanrivera0@gmail.com>
2026-05-22 11:47:41 -0400

    DEBUG: trying to figure out where the extraneous padding is being added, looks like it is added when moving somehow

--- src/lib/bittorrent/bencode/BencodeDecoder.cpp
+++ src/lib/bittorrent/bencode/BencodeDecoder.cpp
@@ -87,8 +87,11 @@ BencodeValue BencodeDecoder::handleString()
     // Advance the index
     pos_ += str_len;
 
-    printf("%.*s", (int)str.size()+ 1, str.c_str());
+    //printf("%.*s", (int)str.size()+ 1, str.c_str());
+std::cout << "size = " << str.size() << "\n";
 
+for (unsigned char c : str)
+    std::cout << (int)c << " ";
     
 
     // Elided
