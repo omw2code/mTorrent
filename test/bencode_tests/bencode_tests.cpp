09280483799fd120b457661bec04f711865e9b8d
Brennan Rivera <brennanrivera0@gmail.com>
2026-05-25 19:50:29 -0400

    Cleanup unit tests for bencoder based on refactor

--- test/bencode_tests/bencode_tests.cpp
+++ test/bencode_tests/bencode_tests.cpp
@@ -11,12 +11,20 @@ TEST(BencodeDecoderString, DecodeString)
     constexpr std::string_view bencode_int{"9:the_thing"};
     bittorrent::BencodeDecoder decoder{};
     decoder.setBencode(bencode_int);
+    
+    /// Our values we want to check
+    bittorrent::BencodeValue actual_data;
+
+    /// Set up the callback
+    bittorrent::BencodeDecoder::Callbacks callbacks;
+    callbacks.on_decode_callback = [&](const bittorrent::BencodeValue &data){ actual_data = data; };
+    decoder.setCallbacks(std::move(callbacks));
 
     /// Decode the thing
-    auto val = decoder.dispatch();
+    decoder.decode();
 
     /// Assert the thing is the thing
-    ASSERT_EQ(std::get<std::string>(val.value), "the_thing");
+    ASSERT_EQ(std::get<std::string>(actual_data.value), "the_thing");
 }
 
 /// Integers are represented by an 'i' followed by the number 
@@ -30,9 +38,19 @@ TEST(BecondeDecoderInt, DecodeInt)
     bittorrent::BencodeDecoder decoder{};
     decoder.setBencode(bencode_uInt);
 
+    /// Decoded value we want to catch
+    bittorrent::BencodeValue actual_data;
+
+    /// Set up callback
+    bittorrent::BencodeDecoder::Callbacks callbacks{};
+    callbacks.on_decode_callback = [&](const bittorrent::BencodeValue &data){ actual_data = data; };
+    decoder.setCallbacks(std::move(callbacks));
+
+    /// Read the data
+    decoder.decode();
+
     /// Check i3e
-    auto val = decoder.dispatch();
-    ASSERT_EQ(std::get<int64_t>(val.value), 3);
+    ASSERT_EQ(std::get<int64_t>(actual_data.value), 3);
 
 }
 
@@ -41,10 +59,20 @@ TEST(BecondeDecoderInt, DecodeNegative)
     constexpr std::string_view bencode_uInt{"i-3e"};
     bittorrent::BencodeDecoder decoder{};
     decoder.setBencode(bencode_uInt);
-    auto val = decoder.dispatch();
+
+    /// Actual data we'll check
+    bittorrent::BencodeValue actual_data;
+
+    /// Set up callback
+    bittorrent::BencodeDecoder::Callbacks callbacks;
+    callbacks.on_decode_callback = [&](const bittorrent::BencodeValue &data){ actual_data = data; };
+    decoder.setCallbacks(std::move(callbacks));
+
+    /// Read the data
+    decoder.decode();
 
     // Check i-3e
-    ASSERT_EQ(std::get<int64_t>(val.value), -3);
+    ASSERT_EQ(std::get<int64_t>(actual_data.value), -3);
 }
 
 TEST(BecondeDecoderInt, DecodeInvalidInt)
@@ -53,11 +81,22 @@ TEST(BecondeDecoderInt, DecodeInvalidInt)
     constexpr std::string_view bencode_Int{"i-03e"};
     bittorrent::BencodeDecoder decoder{};
     decoder.setBencode(bencode_Int);
-    EXPECT_THROW(decoder.dispatch(), std::runtime_error);
 
+    // Thing to check
+    bittorrent::BencodeValue actual_data;
+
+    /// Callbacks
+    bittorrent::BencodeDecoder::Callbacks callbacks;
+    callbacks.on_decode_callback = [&](const bittorrent::BencodeValue &data){ actual_data = data; };
+    decoder.setCallbacks(std::move(callbacks));
+
+    /// This should throw
+    EXPECT_THROW(decoder.decode(), std::runtime_error);
+
+    /// Change it up with a positive number
     constexpr std::string_view bencode_uInt{"i03e"};
     decoder.setBencode(bencode_uInt);
-    EXPECT_THROW(decoder.dispatch(), std::runtime_error);
+    EXPECT_THROW(decoder.decode(), std::runtime_error);
 }
 
 
@@ -66,15 +105,26 @@ TEST(BencodeDecoderInt, DecodeLargeInt)
     constexpr std::string_view bencode_int{"i1000000000000e"};
     bittorrent::BencodeDecoder decoder{};
     decoder.setBencode(bencode_int);
-    auto val = decoder.dispatch();
+   
+    /// Checking this value
+    bittorrent::BencodeValue actual_data;
 
-    ASSERT_EQ(std::get<int64_t>(val.value), 1000000000000);
+    /// Set up callbacks
+    bittorrent::BencodeDecoder::Callbacks callbacks;
+    callbacks.on_decode_callback = [&](const bittorrent::BencodeValue &data){ actual_data = data; };
+    decoder.setCallbacks(std::move(callbacks));
 
+    /// Decode and assert this
+    decoder.decode();
+    ASSERT_EQ(std::get<int64_t>(actual_data.value), 1000000000000);
+
+    /// Set a new piece of data
     constexpr std::string_view bencode_neg{"i-1000000000000e"};
     decoder.setBencode(bencode_neg);
-    val = decoder.dispatch();
 
-    ASSERT_EQ(std::get<int64_t>(val.value), -1000000000000);
+    /// Decode and assert
+    decoder.decode();
+    ASSERT_EQ(std::get<int64_t>(actual_data.value), -1000000000000);
 }
 
 /// Lists are encoded as an 'l' followed by their elements (also bencoded) followed
@@ -84,13 +134,23 @@ TEST(BencodeDecoderList, DecodeStringList)
     constexpr std::string_view bencode_list{"l4:spam4:eggse"};
     bittorrent::BencodeDecoder decoder{};
     decoder.setBencode(bencode_list);
-    auto val = decoder.dispatch();
+
+    /// Thing to check
+    bittorrent::BencodeValue actual_data;
+
+    // Callbacks
+    bittorrent::BencodeDecoder::Callbacks callbacks;
+    callbacks.on_decode_callback = [&](const bittorrent::BencodeValue &data){ actual_data = data; };
+    decoder.setCallbacks(std::move(callbacks));
+
+    // Decode
+    decoder.decode();
 
     /// Smoke test assert size is equal to 2
     using bittorrent_vec = std::vector<bittorrent::BencodeValue>;
     std::vector<std::string> expected{"spam", "eggs"};
-    ASSERT_EQ(std::get<bittorrent_vec>(val.value).size(), expected.size());
-    for (const auto& [ben_val, real_val]  : std::views::zip(std::get<bittorrent_vec>(val.value), expected))
+    ASSERT_EQ(std::get<bittorrent_vec>(actual_data.value).size(), expected.size());
+    for (const auto& [ben_val, real_val]  : std::views::zip(std::get<bittorrent_vec>(actual_data.value), expected))
     {
         using BenBaseType = std::decay_t<decltype(std::get<std::string>(ben_val.value))>;
          static_assert(std::is_same<
@@ -107,10 +167,20 @@ TEST(BencodeDecoderList, DecodeMultiTypeList)
     constexpr std::string_view bencode_list{"l4:spami4e4:eggse"};
     bittorrent::BencodeDecoder decoder{};
     decoder.setBencode(bencode_list);
-    auto val = decoder.dispatch();
+
+    /// Thing to check
+    bittorrent::BencodeValue actual_data;
+
+    /// Set up callbacks
+    bittorrent::BencodeDecoder::Callbacks callbacks;
+    callbacks.on_decode_callback = [&](const bittorrent::BencodeValue &data){ actual_data = data; };
+    decoder.setCallbacks(std::move(callbacks));
+
+    /// Decode the data
+    decoder.decode();
 
     using BittorrentVec = std::vector<bittorrent::BencodeValue>;
-    auto decoded_vec = std::get<BittorrentVec>(val.value);
+    auto decoded_vec = std::get<BittorrentVec>(actual_data.value);
     ASSERT_EQ(decoded_vec.size(), 3);
 
     /// Just grab each value directly and don't worry about iterating through the
@@ -137,13 +207,21 @@ TEST(BencodeDecoderDict, DecodeSimpleDict)
     constexpr std::string_view bencode_dict{"d3:cow3:moo4:spam4:eggse"}; 
     bittorrent::BencodeDecoder decoder{};
     decoder.setBencode(bencode_dict);
-    auto val = decoder.dispatch();
+
+    /// Set up callbacks
+    bittorrent::BencodeValue actual_data;
+    bittorrent::BencodeDecoder::Callbacks callbacks;
+    callbacks.on_decode_callback = [&](const bittorrent::BencodeValue &data){ actual_data = data; };
+    decoder.setCallbacks(std::move(callbacks));
+
+    /// Decode the data
+    decoder.decode();
     
-    using StrMap = std::unordered_map<std::string, std::string>;
+    using StrMap = std::map<std::string, std::string>;
     using BenMap = std::unordered_map<std::string, bittorrent::BencodeValue>;
     StrMap expected { {"cow", "moo"}, {"spam", "eggs"}};
-    ASSERT_EQ(std::get<BenMap>(val.value).size(), expected.size());
-    for (const auto &[ben_kvp, real_kvp] : std::views::zip(std::get<BenMap>(val.value), expected))
+    ASSERT_EQ(std::get<BenMap>(actual_data.value).size(), expected.size());
+    for (const auto &[ben_kvp, real_kvp] : std::views::zip(std::get<BenMap>(actual_data.value), expected))
     {
         ASSERT_EQ(std::get<std::string>(ben_kvp.second.value), real_kvp.second);
     }
